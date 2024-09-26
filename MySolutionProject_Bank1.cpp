79afbe082674a056d0b5b0ee063b4a0bce2b5b7d
hosamkanjo <164902854+hosamkanjo@users.noreply.github.com>
2024-09-26 23:35:54 +0300

    Add files via upload
    this is a bank project   with 6 options include   
    1.Show  Clients    List  (in this window I show my customer information)
    2.Add     New      Client(in this window  you can add a new client)
    3. Delete   Client ((in this window  you can   delete   a client)
    4.Find   Client(in this window you can search for a client )
    [6]Transactions(in this window   you can    Deposit or withdraw  from account any client  )

---
+++ MySolutionProject_Bank1.cpp
@@ -0,0 +1,34 @@
+
+
+// It's My Solution  
+#include <iostream>
+
+
+
+using namespace std;
+
+#include <ctime>
+
+#include<fstream>
+#include<vector>
+#include<cctype>
+#include<string>
+#include <iomanip>
+
+
+// new 
+
+
+
+#include "MySBank1.h"
+using namespace MySBank1;
+
+int main()
+{
+
+
+
+	ShowMainMenue();
+
+
+}
